8c681e6dcdb76f575f5b17f15b2b90d94cfe8ec8
Francesco <119010269+FrankOfTheScience@users.noreply.github.com>
2024-04-20 00:08:00 +0200

    Update constants.js
    Updated customized dialogues

--- src/constants.js
+++ src/constants.js
@@ -1,23 +1,25 @@
 export const scaleFactor = 4;
 
 export const dialogueData = {
-  pc: `This is my PC. I work mostly in JavaScript/TypeScript these days.
-    I've made a couple of games in that language. I also like Golang and Python. Anyway regardless of the language, I just like programming.
-    Here is my <a href="https://github.com/jslegenddev" target="_blank">Github</a>!`,
-  "cs-degree": `This is my CS degree. I hung it on the wall because I'm proud of it. It was a very theoretical degree but I think it gave me a good foundation.`,
-  "sofa-table": `That's my sofa. I like to relax here and watch YouTube. 
-  I also make game programming tutorials on YouTube. Go sub to <a href="https://youtube.com/@jslegenddev" target="_blank">my channel</a>! (If you like the content)
-  You'll learn how I built this portfolio you're currently playing through!`,
-  tv: `That's my TV. I've been watching tech youtubers a lot recently like :
-   <a href="https://www.youtube.com/@ThePrimeTimeagen" target="_blank">Theprimeagen</a>, <a href="https://www.youtube.com/@t3dotgg" target="_blank">Theo - t3.gg</a>,
-  <a href="https://www.youtube.com/@PirateSoftware" target="_blank">PirateSoftware</a> (sometimes) and <a href="https://www.youtube.com/@MelkeyDev" target="_blank">Melkey</a>!`,
-  bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
-  resume: `This is my desk and on it is my resume. <a href="https://github.com/JSLegendDev/Resume/blob/main/JSLegend%20Resume-1.pdf" target="_blank">Check it out?</a>
-  Contact me at jslegend@protonmail.com if you have any interesting job opportunities!`,
-  projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
-  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. Learn more about how to use
-  Kaboom.js by watching some of my tutorials <a href="https://youtube.com/@jslegenddev" target="_blank">here</a>.`,
-  library: `There are a lot of programming books on my shelves. There is even one in French (I also speak French btw).
-  I probably only read one of them. Who else compulsively buys technical books without ever finishing them?`,
-  exit: `If you want to exit JSLegendDev's portfolio, just close the tab.`,
+  pc: `This is where I have spent most of the days of my life, since I was a child! My first approaches with programming have been at
+  13 years old, with Arduino, I was trying do create a little Nuclear Power Plant demo model for the middle school final examination... 
+  unfortunately the project was a bit too ambitious to me.
+  I learned a bit of python and C# at University, but then I discovered Java and C# and focused on these.
+  Right now I am a backend and cloud developer, specialized in .NET and Azure Cloud, but I also know a bit of frontend development.
+ `,
+  tv: `That's my entertainment content!
+  I love listening and playing Classic Rock and Heavy Metal, that's one of my favourite <a href="https://www.youtube.com/watch?v=M0TcB5lxfuY" target="_blank">song</a>!
+  Other honorable mentions about my passions are backpacking around the world, surfing, astronomy and rockets!
+  Looking forward to see <a href="https://www.youtube.com/watch?v=921VbEMAwwY" target="_blank">this beauty</a> reach Mars with humans on it!`,
+  bed: `Well... even elves and cyborgs need a rest or a reboot sometimes.
+  Maybe we might be at the end of my presentation (well, it depends the order you explored the house actually)
+  but let's keep in touch! You can find my contacts in my resumee,
+  it is on the table if you haven't seen it yet!`,
+  resume: `Oh, who would have thought!
+  This paper on the desk is actually <a href="https://github.com/FrankOfTheScience/Resumee/blob/master/CV%20-%20Francesco%20Dell'Ascenza.pdf" target="_blank">my resumee!</a>
+  You can contact me at the email or phone number that you can find in it, if you have any interesting job opportunities!`,
+  projects: `Those are... projects.. I guess... Well they are quite in a mess! Here is my <a href="https://github.com/FrankOfTheScience" target="_blank">Github</a>, there you can find them tidied up!`,
+  library: `A library, other than a bunch of NuGet packages here you can find some books that changed my life!
+  One's about Feynman's life told through anecdotes, another one is by Sagan but I also have Tolkien and Steinbeck on my shelves!`,
+  exit: `If you want to exit my portfolio, just close the tab :)`,
 };
